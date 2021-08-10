0da4ccf23d66502d1dd05bcf9b7dcc401e70958d
hamza887 <hamzabutt887@hotmail.com>
2021-08-10 15:20:31 -0700

    NAVBAR completed

--- client/src/Topbar/Topbar.js
+++ client/src/Topbar/Topbar.js
@@ -2,6 +2,30 @@ import './topbar.css'
 
 export default function TopBar(){
    return(
-       <h1>hello</h1>
+       <div className='top'>
+           <div className='topLeft'>
+           <i className="topIcon fab fa-facebook-square"></i>
+           <i className="topIcon fab fa-twitter-square"></i>
+           <i className="topIcon fab fa-pinterest-square"></i>
+           <i className="topIcon fab fa-instagram-square"></i>
+
+           </div>
+           <div className='topCenter'>
+            <div className='topList'>
+                <li className='topListItem'>HOME</li>
+                <li className='topListItem'>ABOUT</li>
+                <li className='topListItem'>CONTACT</li>
+                <li className='topListItem'>WRITE</li>
+                <li className='topListItem'>LOGOUT</li>
+
+
+
+            </div>
+           </div>
+           <div className='topRight'>
+               <img className='topImg' src='https://dpssahiwal.edu.pk/wp-content/uploads/elementor/thumbs/dps-pic-scaled-ony6hsllxpuir4t5fewkp6n9l17mq58zxp4j2p96lc.jpeg' alt=''></img>
+               <i className="topSearchIcon fas fa-search"></i>
+           </div>
+       </div>
    )
 }
