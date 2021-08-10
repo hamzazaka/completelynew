11d84b7a2cbdd3d9972980b36cda241c962ca146
hamza887 <hamzabutt887@hotmail.com>
2021-08-10 15:25:17 -0700

    creating new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import TopBar from './Topbar/Topbar';
+import Header from './header/header'
 
 function App() {
   return (
+    <>
    <TopBar/>
+  <Header/>
+   </>
+
+
   );
 }
 
