1bfaae94a0971ad02c82fa226113f9c23c39befe
hamza887 <hamzabutt887@hotmail.com>
2021-08-10 21:49:26 -0700

    working on login and registeration

--- api/index.js
+++ api/index.js
@@ -2,19 +2,18 @@
  const app=express();
  const dotenv=require('dotenv');
  const mongoose=require('mongoose');
+ const authRoute=require('./routes/auth')
 
  dotenv.config();
-
+ app.use(express.json())
  mongoose.connect(process.env.MONGO_URL,{
      useNewUrlParser:true,
      useUnifiedTopology:true,
      useCreateIndex:true
  }).then(console.log('connnected to mongo')).catch((err)=>console.log(err));
 
- app.use('/',(req,res)=>{
-     console.log('hello thi is homepage');
- })
 
+app.use('/api/auth',authRoute)
 
  app.listen('5000',()=>{
      console.log('backend is running');
