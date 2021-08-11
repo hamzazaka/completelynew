7cab7acf9d95368e5ba4e205fee367d56638bee8
hamza887 <hamzabutt887@hotmail.com>
2021-08-11 06:57:39 -0700

    version 3 is done i guess

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,32 @@
-
-import './App.css';
-import TopBar from './Topbar/Topbar';
-import Home from './pages/home/Home';
-import Single from './pages/single/Single';
-import Write from './pages/write/Write';
-import Settings from './pages/settings/Setting';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import Home from "./pages/home/Home";
+import TopBar from "../src/Topbar/Topbar"
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Setting";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  const user=false;
+  const { user } = useContext(Context);
   return (
     <Router>
-   <TopBar/>
-   <Switch>
-     <Route exact path='/'>
-       <Home/>
-     </Route>
-     <Route path='/register'>
-      {user?<Home/>:<Register/>}
-     </Route>
-     <Route path='/write'>
-     {user?<Write/>:<Register/>}
-       </Route>
-       <Route path='/settings'>
-       {user?<Settings/>:<Register/>}
-       </Route>
-     <Route path='/login'>
-     {user?<Home/>:<Login/>}
-     </Route>
-     <Route path='/post/:postId'>
-       <Single/>
-       </Route>
-   </Switch>
-   </Router>
-
-
+      <TopBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
