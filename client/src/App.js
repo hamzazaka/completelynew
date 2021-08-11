4c6058f22de6f99a41f777f8ed31c8501ab12dee
hamza887 <hamzabutt887@hotmail.com>
2021-08-10 17:42:50 -0700

    working on write component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import TopBar from './Topbar/Topbar';
 import Home from './pages/home/Home';
 import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
   return (
     <>
    <TopBar/>
-    <Single/>
+   <Write/>
+
    </>
 
 
