8fc846bdb7b0d0bca80f1a5ad8ea78066039f001
hamza887 <hamzabutt887@hotmail.com>
2021-08-10 18:04:45 -0700

    working on SETTING component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import TopBar from './Topbar/Topbar';
 import Home from './pages/home/Home';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
+import Settings from './pages/settings/Setting';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
    <TopBar/>
-   <Write/>
-
+   <Login/>
    </>
 
 
