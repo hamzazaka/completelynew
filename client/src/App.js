291c0456498c9f20ded409de901009b504562c08
hamza887 <hamzabutt887@hotmail.com>
2021-08-10 19:01:21 -0700

    working on some of the issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TopBar from './Topbar/Topbar';
 import Home from './pages/home/Home';
@@ -7,7 +7,7 @@ import Write from './pages/write/Write';
 import Settings from './pages/settings/Setting';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import {BrowserRouter as Router, Switch,Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 function App() {
   const user=false;
